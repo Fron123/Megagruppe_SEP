b0b2a08ade8a5c205bf9ee28b4d64f75232d7667
Fron123 <80124441+Fron123@users.noreply.github.com>
2021-06-03 19:44:52 +0200

    Update Stepsize_Control_ADOL-C.cpp

--- Stepsize/Stepsize_Control_ADOL-C.cpp
+++ Stepsize/Stepsize_Control_ADOL-C.cpp
@@ -1,1 +1,33 @@
+// Stepsize Control Florian
 
+#pragma once 
+
+#include<iostream>
+
+#include<Eigen/Dense>
+#include "adolc.h"
+
+template<typename T,int N>
+typename Nonlinear::System<T,N>::VT Stepsize(
+        const typename Nonlinear::System<T,N>::MT& A,
+        const typename Nonlinear::System<T,N>::VT& b,
+//        const int shrinking_factor,
+//        const int alpha_max
+//      const int c1;
+//      const int c2;
+)       
+        {
+        
+        const typename Nonlinear::System<T,N>::MT** Hessian;
+        const typename Nonlinear::System<T,N>::VT** Gradient;
+  
+        hessian(1,N,A,Hessian);
+        gradient(1,N,A,Gradient);
+        
+        t = Gradient/Hessian;
+        step = b(0) + t;
+       
+        
+        return step;
+        
+        }
